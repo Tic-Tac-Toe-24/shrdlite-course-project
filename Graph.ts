d7af13509db4604bcae93676966046142c35a485
Philémon <philemon.bouzy@gmail.com>
2016-04-27 19:33:09 +0200

    Collections imports and refatoring

--- Graph.ts
+++ Graph.ts
@@ -1,6 +1,12 @@
 ///<reference path="lib/collections.ts"/>
 ///<reference path="lib/node.d.ts"/>
 
+import ICompareFunction = collections.ICompareFunction;
+import Heap = collections.Heap;
+import Set = collections.Set;
+import Dictionary = collections.Dictionary;
+import LinkedList = collections.LinkedList;
+
 /** Graph module
 *
 *  Types for generic A\* implementation.
@@ -22,7 +28,7 @@ interface Graph<Node> {
     /** Computes the edges that leave from a node. */
     outgoingEdges(node : Node) : Edge<Node>[];
     /** A function that compares nodes. */
-    compareNodes : collections.ICompareFunction<Node>;
+    compareNodes : ICompareFunction<Node>;
 }
 
 /** Type that reports the result of a search. */
@@ -49,81 +55,72 @@ class SearchResult<Node> {
 * @returns A search result, which contains the path from `start` to a node satisfying `goal` and the cost of this path.
 */
 function aStarSearch<Node> (
-    graph : Graph<Node>,
-    start : Node,
-    goal : (n:Node) => boolean,
-    heuristics : (n:Node) => number,
-    timeout : number
+  graph : Graph<Node>,
+  start : Node,
+  goal : (n:Node) => boolean,
+  heuristics : (n:Node) => number,
+  timeout : number
 ) : SearchResult<Node> {
-    // A dummy search result: it just picks the first possible neighbour
-    var result : SearchResult<Node> = {
-        path: [start],
-        cost: 0
-    };
+  // A dummy search result: it just picks the first possible neighbour
+  var result : SearchResult<Node> = {
+      path: [start],
+      cost: 0
+  };
 
+  var openNodes : Heap<Node> = new Heap<Node>(compareNodes);
+  var closedNodes : Set<Node> = new Set<Node>();
+  var costs : Dictionary<Node, number> = new Dictionary<Node, number>();
+  var predecessors : Dictionary<Node, Node> = new Dictionary<Node, Node>();
+  openNodes.add(start);
 
+  var getF = function(node : Node) : number{
+      return heuristics(node) + costs.getValue(node);
+  }
 
-    var openNodes : collections.Heap<Node> = new collections.Heap<Node>(compareFValue);
-    var closedNodes : collections.Set<Node>;
-    var costs : collections.Dictionary<Node, number>;
-    var predecessors : collections.Dictionary<Node, Node>;
-    openNodes.add(start);
+  var compareNodes : ICompareFunction<Node> = function(first, second) {
+      return getF(firstNode) - getF(secondNode);
+  }
 
+  while (openNodes.size() > 0) {
+    var currentN = openNodes.removeRoot();
+    console.log("one node removed");
+    if(goal(currentN)) {
+      var path : LinkedList<Node> = new LinkedList<Node>();
+      while(!path.contains(start)) { //collect path nodes
+        path.add(currentN);
+        currentN = predecessors.getValue(currentN);
+      }
 
-    var getF = function(node : Node) : number{
-        return heuristics(node) + costs.getValue(node);
+      // reverse to create path
+      result.path = path.toArray().reverse();
+
+      result.cost = costs.getValue(currentN);
+      console.log(result);
+
+      break;
     }
 
-    var compareFValue : collections.ICompareFunction<Node> = function(node1 : Node, node2 : Node) {
-        return getF(node1) - getF(node2);
+    for (var edge of graph.outgoingEdges(currentN)) {
+      if(!closedNodes.contains(edge.to)) {
+        //openNodes.add(edge.to);
+        if(!openNodes.contains(edge.to)
+          || costs.getValue(currentN) + edge.cost < costs.getValue(edge.to)) {
+          predecessors.setValue(edge.to, currentN);
+          costs.setValue(edge.to, costs.getValue(currentN) + edge.cost);
+          if(!openNodes.contains(edge.to)) {
+            openNodes.add(edge.to);
+            console.log("one node added");
+          }
+          else {
+            //shift / bubble up
+          }
+        }
+      }
     }
+    closedNodes.add(currentN);
 
-
-    while (openNodes.size() > 0) {
-        var currentN = openNodes.removeRoot();
-        console.log("one node removed");
-        if(goal(currentN)) {
-          var path : collections.LinkedList<Node> = new collections.LinkedList<Node>();
-          while(!path.contains(start)) { //collect path nodes
-            path.add(currentN);
-            currentN = predecessors.getValue(currentN);
-          }
-
-          // reverse to create path
-          result.path = path.toArray().reverse();
-
-          result.cost = costs.getValue(currentN);
-          console.log(result);
-
-
-          break;
-        }
-        for (var edge of graph.outgoingEdges(currentN)) {
-          if(!closedNodes.contains(edge.to)) {
-            //openNodes.add(edge.to);
-            if(!openNodes.contains(edge.to) || costs.getValue(currentN) + edge.cost < costs.getValue(edge.to)) {
-              predecessors.setValue(edge.to, currentN);
-              costs.setValue(edge.to, costs.getValue(currentN) + edge.cost);
-              if(!openNodes.contains(edge.to)) {
-                openNodes.add(edge.to);
-                console.log("one node added");
-              }
-              else {
-                //shift / bubble up
-              }
-            }
-
-
-
-          }
-        }
-        closedNodes.add(currentN);
-
-
-        //initialize the open list
-
-
-/*while the open list is not empty
+  /*
+  while the open list is not empty
     find the node with the least f on the open list, call it "q"
     pop q off the open list
     generate q's 8 successors and set their parents to q
@@ -140,14 +137,8 @@ function aStarSearch<Node> (
         otherwise, add the node to the open list
     end
     push q on the closed list
-end
-*/
-
-        //var edge : Edge<Node> = graph.outgoingEdges(start) [0];
-        //if (! edge) break;
-        //start = edge.to;
-        //result.path.push(start);
-        //result.cost += edge.cost;
-    }
-    return result;
+  end
+  */
+  }
+  return result;
 }
